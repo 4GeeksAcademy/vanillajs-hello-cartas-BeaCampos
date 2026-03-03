c11a0803626a4bdaed603f3f0a46bf8b560c8235
beatriz24BCN <beatriz24bcn@hotmail.com>
2026-03-03 12:19:13 +0000

    projecto finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-import "bootstrap";
-import "./style.css";
+let suits = ["♥", "♦", "♠", "♣"];
+let suitClasses = ["heart", "diamond", "spade", "club"];
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function generateCard() {
+
+  let randomSuitIndex = Math.floor(Math.random() * suits.length);
+  let randomValueIndex = Math.floor(Math.random() * values.length);
+
+  let suit = suits[randomSuitIndex];
+  let suitClass = suitClasses[randomSuitIndex];
+  let randomValue = values[randomValueIndex];
+
+  let card = document.getElementById("card");
+  let topSuit = document.querySelector(".top-suit");
+  let bottomSuit = document.querySelector(".bottom-suit");
+  let value = document.querySelector(".value");
+
+  card.className = "card";
+  card.classList.add(suitClass);
+
+  topSuit.innerHTML = suit;
+  bottomSuit.innerHTML = suit;
+  value.innerHTML = randomValue;
+}
+
+window.onload = generateCard;
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
